42ad96163fe369d93b93ae139726c5c1f858df02
theluposki <theluposki@gmail.com>
2024-06-20 07:53:23 -0300

    verdade

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import { perguntas } from "./mocks/perguntas.js";
 
-const verdades = ref(perguntas)
+const frase = ref("")
+
+function getAleatoriaFrase() {
+    const tipo = Math.random() < 0.5? 'verdades' : 'desafios';
+    const indice = Math.floor(Math.random() * perguntas[tipo].length);
+    return perguntas[tipo][indice];
+}
+
+const girar = () => frase.value = getAleatoriaFrase()
+
+onMounted(() => {
+    frase.value = getAleatoriaFrase()
+})
 </script>
 
 <template>
  <h1 class="poetsen ft-pink">Hot_challenges</h1>
- <p class="panel-text nanum ft-blue ft-22">{{ verdades.verdade[2].pergunta }}</p>
- <p class="zeyada ft-blue ft-30 align-right">verdade</p>
+ <p class="panel-text nanum ft-blue ft-22">{{ frase.pergunta }}</p>
+ <p class="zeyada ft-blue ft-30 align-right">{{ frase.categoria }}</p>
+
+ <button class="btn" @click="girar">Girar</button>
 </template>
--- src/mocks/perguntas.js
+++ src/mocks/perguntas.js
@@ -1,207 +1,247 @@
 export const perguntas = {
-    verdade: [
+    verdades: [
         {
             id: 1,
             pergunta: "Você transaria com alguém por dinheiro? Se sim, por quanto?",
-            aceito: true
+            aceito: true,
+            categoria: "pergunta"
         },
         {
             id: 2,
             pergunta: "Você prefere transar de manhã, a tarde ou a noite?",
-            aceito: true
+            aceito: true,
+            categoria: "pergunta"
         },
         {
             id: 3,
             pergunta: "Que tipo de pessoa você é quando está bêbado? Feliz? Deprimido? Apaixonado? Chato?",
-            aceito: true
+            aceito: true,
+            categoria: "pergunta"
         },
         {
             id: 4,
             pergunta: "Você prefere transar com a luz acesa ou apagada?",
-            aceito: true
+            aceito: true,
+            categoria: "pergunta"
         },
         {
             id: 5,
             pergunta: "Qual foi a maior loucura que você já fez quando estava bêbado(a)?",
-            aceito: true
+            aceito: true,
+            categoria: "pergunta"
         },
         {
             id: 6,
             pergunta: "Você já fez algo ilegal?",
-            aceito: true
+            aceito: true,
+            categoria: "pergunta"
         },
         {
             id: 7,
             pergunta: "Seu(ua) melhor amigo(a) tá levando chifre. Você conta ou não conta?",
-            aceito: true
+            aceito: true,
+            categoria: "pergunta"
         },
         {
             id: 8,
             pergunta: "Com qual celebridade você transaria se pudesse escolher?",
-            aceito: true
+            aceito: true,
+            categoria: "pergunta"
         },
         {
             id: 9,
             pergunta: "Na sua opinião, qual a melhor música para se fazer um striptease?",
-            aceito: true
+            aceito: true,
+            categoria: "pergunta"
         },
         {
             id: 10,
             pergunta: "Se você ganhasse R$ 3000,00 hoje, como gastaria esse dinheiro?",
-            aceito: true
+            aceito: true,
+            categoria: "pergunta"
         },
         {
             id: 11,
             pergunta: "Você já beijou alguém do mesmo sexo?",
-            aceito: true
+            aceito: true,
+            categoria: "pergunta"
         },
         {
             id: 12,
             pergunta: "O que é que o outro jogador faz que te excita na hora?",
-            aceito: true
+            aceito: true,
+            categoria: "pergunta"
         },
         {
             id: 13,
             pergunta: "Por qual quantia de dinheiro você aceitaria posar nu?",
-            aceito: true
+            aceito: true,
+            categoria: "pergunta"
         },
         {
             id: 14,
             pergunta: "Você já transou em algum lugar público? Se sim qual?",
-            aceito: true
+            aceito: true,
+            categoria: "pergunta"
         },
         {
             id: 15,
             pergunta: "Qual é a sua parte preferida do corpo do outro jogador?",
-            aceito: true
+            aceito: true,
+            categoria: "pergunta"
         },
         {
             id: 16,
             pergunta: "Se você pudesse voltar ao passado e dar um conselho para você antes da sua primeira vez, qual seria?",
-            aceito: true
+            aceito: true,
+            categoria: "pergunta"
         },
         {
             id: 17,
             pergunta: "Se você tivesse que beijar um dos jogadores quem seria?",
-            aceito: true
+            aceito: true,
+            categoria: "pergunta"
         },
         {
             id: 18,
             pergunta: "Com que idade você assistiu o seu primeiro pornô?",
-            aceito: true
+            aceito: true,
+            categoria: "pergunta"
         },
         {
             id: 19,
             pergunta: "Como foi a sua primeira vez?",
-            aceito: true
+            aceito: true,
+            categoria: "pergunta"
         },
         {
             id: 20,
             pergunta: "Os jogadores podem fazer a pergunta que quiserem e você tem que responder honestamente.",
-            aceito: true
+            aceito: true,
+            categoria: "pergunta"
         },
 
     ],
     desafios: [
         {
             id: 1,
-            desafio: "Chupe um dos dedos do outro jogador.",
-            aceito: true
+            pergunta: "Chupe um dos dedos do outro jogador.",
+            aceito: true,
+            categoria: "desafio"
         },
         {
             id: 2,
-            desafio: "De uma mordidinha na orelha do outro jogador.",
-            aceito: true
+            pergunta: "De uma mordidinha na orelha do outro jogador.",
+            aceito: true,
+            categoria: "desafio"
         },
         {
             id: 3,
-            desafio: "Faça uma mímica da ação que você gostaria de ver o outro jogador fazer mais tarde.",
-            aceito: true
+            pergunta: "Faça uma mímica da ação que você gostaria de ver o outro jogador fazer mais tarde.",
+            aceito: true,
+            categoria: "desafio"
         },
         {
             id: 4,
-            desafio: "De um beijo no peito do outro jogador.",
-            aceito: true
+            pergunta: "De um beijo no peito do outro jogador.",
+            aceito: true,
+            categoria: "desafio"
         },
         {
             id: 5,
-            desafio: "Massageie por 45 segundos as costas do outro jogador.",
-            aceito: true
+            pergunta: "Massageie por 45 segundos as costas do outro jogador.",
+            aceito: true,
+            categoria: "desafio"
         },
         {
             id: 6,
-            desafio: "Troque de camiseta com o outro jogador.",
-            aceito: true
+            pergunta: "Troque de camiseta com o outro jogador.",
+            aceito: true,
+            categoria: "desafio"
         },
         {
             id: 7,
-            desafio: "Você deve convencer o outro jogador a te dar um beijo no pescoço.",
-            aceito: true
+            pergunta: "Você deve convencer o outro jogador a te dar um beijo no pescoço.",
+            aceito: true,
+            categoria: "desafio"
         },
         {
             id: 8,
-            desafio: "Tire imediatamente a peça de roupa que preferir.",
-            aceito: true
+            pergunta: "Tire imediatamente a peça de roupa que preferir.",
+            aceito: true,
+            categoria: "desafio"
         },
         {
             id: 9,
-            desafio: "Tire uma peça de roupa escolhida pelo outro jogador.",
-            aceito: true
+            pergunta: "Tire uma peça de roupa escolhida pelo outro jogador.",
+            aceito: true,
+            categoria: "desafio"
         },
         {
             id: 10,
-            desafio: "Beije as costas do outro jogador desenhando um número, se ele não adivinhar ele leva um tapa na bunda.",
-            aceito: true
+            pergunta: "Beije as costas do outro jogador desenhando um número, se ele não adivinhar ele leva um tapa na bunda.",
+            aceito: true,
+            categoria: "desafio"
         },
         {
             id: 11,
-            desafio: "Acaricie delicadamente os cabelos do outro jogador, depois beije-o segurando o pescoço.",
-            aceito: true
+            pergunta: "Acaricie delicadamente os cabelos do outro jogador, depois beije-o segurando o pescoço.",
+            aceito: true,
+            categoria: "desafio"
         },
         {
             id: 12,
-            desafio: "Dê vários beijinhos na mão do outro jogador.",
-            aceito: true
+            pergunta: "Dê vários beijinhos na mão do outro jogador.",
+            aceito: true,
+            categoria: "desafio"
         },
         {
             id: 13,
-            desafio: "Suba em cima do outro jogador e movimente-se como se estivesse transando.",
-            aceito: true
+            pergunta: "Suba em cima do outro jogador e movimente-se como se estivesse transando.",
+            aceito: true,
+            categoria: "desafio"
         },
         {
             id: 14,
-            desafio: "Sente numa cadeira e se renda. Seus olhos serão vendados pelo outro jogador que poderá te beijar onde ele quiser.",
-            aceito: true
+            pergunta: "Sente numa cadeira e se renda. Seus olhos serão vendados pelo outro jogador que poderá te beijar onde ele quiser.",
+            aceito: true,
+            categoria: "desafio"
         },
         {
             id: 15,
-            desafio: "Jogue o outro jogador na parede e sussurre em seu ouvido todos os seus desejos.",
-            aceito: true
+            pergunta: "Jogue o outro jogador na parede e sussurre em seu ouvido todos os seus desejos.",
+            aceito: true,
+            categoria: "desafio"
         },
         {
             id: 16,
-            desafio: "Deixe o outro jogador vendar seus olhos, você irá experimentar uma bebida, se você adivinhar qual é a bebida poderá pedir para que o outro jogador dê um beijo num lugar de sua escolha.",
-            aceito: true
+            pergunta: "Deixe o outro jogador vendar seus olhos, você irá experimentar uma bebida, se você adivinhar qual é a bebida poderá pedir para que o outro jogador dê um beijo num lugar de sua escolha.",
+            aceito: true,
+            categoria: "desafio"
         },
         {
             id: 17,
-            desafio: "Beije a parte inferior da barriga do outro jogador.",
-            aceito: true
+            pergunta: "Beije a parte inferior da barriga do outro jogador.",
+            aceito: true,
+            categoria: "desafio"
         },
         {
             id: 18,
-            desafio: "Você não pode se mexer por um minuto. Enquanto isso o outro jogador pode fazer o que quiser com você.",
-            aceito: true
+            pergunta: "Você não pode se mexer por um minuto. Enquanto isso o outro jogador pode fazer o que quiser com você.",
+            aceito: true,
+            categoria: "desafio"
         },
         {
             id: 19,
-            desafio: "Descubra a cor da roupa íntima do outro jogador. Se você adivinhar poderá tirar uma peça de roupa dele(a).",
-            aceito: true
+            pergunta: "Descubra a cor da roupa íntima do outro jogador. Se você adivinhar poderá tirar uma peça de roupa dele(a).",
+            aceito: true,
+            categoria: "desafio"
         },
         {
             id: 20,
-            desafio: "Passe um cubo de gelo na parte que vc acha mais gostosa do outro jogador.",
-            aceito: true
+            pergunta: "Passe um cubo de gelo na parte que vc acha mais gostosa do outro jogador.",
+            aceito: true,
+            categoria: "desafio"
         },
     ]
 }
